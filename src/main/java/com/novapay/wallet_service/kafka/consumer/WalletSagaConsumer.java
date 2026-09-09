07e6a9819641492ada3e0166d89220a1ac69cc4f
Azaz Khan <azazkhan.it07@gmail.com>
2026-09-09 13:21:39 +0530

    Implement Kafka retry and DLT handling

--- src/main/java/com/novapay/wallet_service/kafka/consumer/WalletSagaConsumer.java
+++ src/main/java/com/novapay/wallet_service/kafka/consumer/WalletSagaConsumer.java
@@ -11,13 +11,21 @@ import com.novapay.wallet_service.saga.event.WalletRefundedEvent;
 import com.novapay.wallet_service.service.WalletService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
+@RetryableTopic(
+        attempts = "4",
+        backoff = @Backoff(
+                delay = 2000,
+                multiplier = 2.0))
 @KafkaListener(
         topics = KafkaTopics.WALLET_COMMANDS,
         groupId = "wallet-saga-group")
@@ -67,5 +75,10 @@ public class WalletSagaConsumer{
                         command.paymentReference(),
                         command.payerWalletId(),
                         command.amount()));
+    }
+
+    @DltHandler
+    public void handleDlt(Object event) {
+        log.error("Message moved to DLT after retries exhausted. Event: {}", event);
     }
 }
